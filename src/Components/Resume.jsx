ec8f21f496b30dae9645e97ecb4a15dd683ec811
aGRAWAL <gargbhoomi01@gmail.com>
2025-03-15 18:07:35 +0530

    Resume section

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { FiExternalLink, FiDownload } from "react-icons/fi";
+import '../Styles/Resume.css';
 const Resume = () => {
   return (
-    <div>
-      <p>Hello, I am Resume Section</p>
+    <div id="outer-div">
+      <h1>Resume</h1>
+      <div id="online-div">
+      <a href="/Resume_Bhoomi.pdf" target='_blank' rel='noopener noreferrer' className='resume_link'><FiExternalLink className='icon'/>View Resume Online</a>
+      </div>
+      <div id="download-div">
+      <a href='/Resume_Bhoomi.pdf' className='resume_link' download='Bhoomi_Garg_Resume'><FiDownload className='icon'/>Download Resume</a>
+      </div>
     </div>
   )
 }
